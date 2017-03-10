93130336ceabb100efdf0e5466e1efcf03d54d25
James Kyle <me@thejameskyle.com>
2017-03-09 23:47:10 -0800

    Only use polyfill when necessary

--- polyfill.js
+++ polyfill.js
@@ -1,41 +1,51 @@
-const PromiseStatus = Symbol('[[PromiseStatus]]');
-const PromiseValue = Symbol('[[PromiseValue]]');
+((root) => {
+  if (
+    typeof root.Promise === 'function' &&
+    typeof root.Promise.isPending === 'function' &&
+    typeof root.Promise.isResolved === 'function' &&
+    typeof root.Promise.isRejected === 'function' &&
+    typeof root.Promise.getValue === 'function'
+  ) {
+    const PromiseStatus = Symbol('[[PromiseStatus]]');
+    const PromiseValue = Symbol('[[PromiseValue]]');
 
-const StatusPending = Symbol('pending');
-const StatusResolved = Symbol('resolved');
-const StatusRejected = Symbol('resolved');
+    const StatusPending = Symbol('pending');
+    const StatusResolved = Symbol('resolved');
+    const StatusRejected = Symbol('resolved');
 
-class PolyfillPromise extends Promise {
-  constructor(executor, ...args) {
-    super((resolve, reject) => {
-      executor(value => {
-        this[PromiseStatus] = StatusResolved;
-        this[PromiseValue] = value;
-        return resolve(value);
-      }, value => {
-        this[PromiseStatus] = StatusRejected;
-        this[PromiseValue] = value;
-        return reject(value);
-      });
-    }, ...args);
-    this[PromiseStatus] = StatusPending;
+    class PolyfillPromise extends root.Promise {
+      constructor(executor, ...args) {
+        super((resolve, reject) => {
+          executor(value => {
+            this[PromiseStatus] = StatusResolved;
+            this[PromiseValue] = value;
+            return resolve(value);
+          }, value => {
+            this[PromiseStatus] = StatusRejected;
+            this[PromiseValue] = value;
+            return reject(value);
+          });
+        }, ...args);
+        this[PromiseStatus] = StatusPending;
+      }
+
+      static isPending(promise) {
+        return promise[PromiseStatus] === StatusPending;
+      }
+
+      static isResolved(promise) {
+        return promise[PromiseStatus] === StatusResolved;
+      }
+
+      static isRejected(promise) {
+        return promise[PromiseStatus] === StatusRejected;
+      }
+
+      static getValue(promise) {
+        return promise[PromiseValue];
+      }
+    }
+
+    root.Promise = PolyfillPromise;
   }
-
-  static isPending(promise) {
-    return promise[PromiseStatus] === StatusPending;
-  }
-
-  static isResolved(promise) {
-    return promise[PromiseStatus] === StatusResolved;
-  }
-
-  static isRejected(promise) {
-    return promise[PromiseStatus] === StatusRejected;
-  }
-
-  static getValue(promise) {
-    return promise[PromiseValue];
-  }
-}
-
-global.Promise = PolyfillPromise;
+}(this));
